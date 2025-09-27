2d3be4625dcc15d5bd918b4c4a3de34a03372ea9
s0hum <sohum.p.gupta@gmail.com>
2025-09-26 20:27:19 -0700

    added nextbindings + intake subsystem and teleop for nextftc

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/subsystems/Intake.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.commandbase.subsystems;
+
+import com.bylazar.configurables.annotations.Configurable;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.feedback.PIDCoefficients;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.hardware.controllable.RunToVelocity;
+import dev.nextftc.hardware.impl.MotorEx;
+
+@Configurable
+public class Intake implements Subsystem {
+
+    public static double intakeP = 1;
+    public static double intakeI = 0;
+    public static double intakeD = 0.005;
+
+    public final static Intake INSTANCE = new Intake();
+
+    private Intake() { }
+
+    private final MotorEx intakeMotor = new MotorEx("intakeMotor");
+
+    private final PIDCoefficients intakePIDComponents = new PIDCoefficients(intakeP, intakeI, intakeD);
+
+    private final ControlSystem intakeController = ControlSystem.builder()
+            .velPid(intakePIDComponents)
+            .build();
+
+    public Command intakeOff = new RunToVelocity(intakeController, 0).requires(this);
+
+    public Command intakeHalfSpeed = new RunToVelocity(intakeController, 500).requires(this);
+
+    public Command intakeFullSpeed = new RunToVelocity(intakeController, 1000).requires(this);
+
+    public Command intakeReverseHalfSpeed = new RunToVelocity(intakeController, -500).requires(this);
+
+    public Command intakeReverseFullSpeed = new RunToVelocity(intakeController, -1000).requires(this);
+
+    @Override
+    public void periodic() {
+        intakeMotor.setPower(intakeController.calculate(intakeMotor.getState()));
+        ActiveOpMode.telemetry();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/V1_Teleop.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.commandbase.subsystems.Intake;
+
+import dev.nextftc.bindings.BindingManager;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.driving.FieldCentric;
+import dev.nextftc.hardware.driving.HolonomicMode;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.Direction;
+import dev.nextftc.hardware.impl.IMUEx;
+import dev.nextftc.hardware.impl.MotorEx;
+import static dev.nextftc.bindings.Bindings.*;
+
+@TeleOp
+@Configurable //panels
+public class V1_Teleop extends NextFTCOpMode {
+
+    public V1_Teleop() {
+        addComponents( //add needed components
+                new SubsystemComponent(Intake.INSTANCE),
+                BulkReadComponent.INSTANCE,
+                BindingsComponent.INSTANCE
+        );
+    }
+
+    //create motors and set directions
+    private final MotorEx frontLeftMotor = new MotorEx("frontLeftMotor").reversed();
+    private final MotorEx frontRightMotor = new MotorEx("frontRightMotor");
+    private final MotorEx backLeftMotor = new MotorEx("backLeftMotor").reversed();
+    private final MotorEx backRightMotor = new MotorEx("backRightMotor");
+    private final IMUEx imu = new IMUEx("imu", Direction.UP, Direction.FORWARD).zeroed();
+
+    @Override
+    public void onStartButtonPressed() {
+        Command driverControlled = new MecanumDriverControlled(
+                frontLeftMotor,
+                frontRightMotor,
+                backLeftMotor,
+                backRightMotor,
+                Gamepads.gamepad1().leftStickY(),
+                Gamepads.gamepad1().leftStickX(),
+                Gamepads.gamepad1().rightStickX(),
+                new FieldCentric(imu)
+        );
+        driverControlled.schedule();
+
+        Gamepads.gamepad1().a()
+                .whenBecomesTrue(Intake.INSTANCE.intakeFullSpeed);
+        Gamepads.gamepad1().y()
+                .whenBecomesTrue(Intake.INSTANCE.intakeOff);
+    }
+
+    @Override
+    public void onUpdate() {
+        ActiveOpMode.telemetry();
+        BindingManager.update();
+    }
+
+    @Override
+    public void onStop() {
+        BindingManager.reset();
+    }
+}
