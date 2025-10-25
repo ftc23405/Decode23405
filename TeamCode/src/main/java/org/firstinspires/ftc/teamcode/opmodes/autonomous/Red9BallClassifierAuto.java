d0150729f4b9efed7518a7e810d6ba06bda788cd
s0hum <sohum.p.gupta@gmail.com>
2025-10-24 17:13:05 -0700

    redid classifier auto paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Red9BallClassifierAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Red9BallClassifierAuto.java
@@ -8,15 +8,11 @@ import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.Path;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.commandbase.subsystems.Intake;
-import org.firstinspires.ftc.teamcode.commandbase.subsystems.Shooter;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.WaitUntil;
 import dev.nextftc.core.commands.groups.SequentialGroup;
-import dev.nextftc.core.commands.utility.InstantCommand;
-import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.extensions.pedro.FollowPath;
 import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
@@ -24,11 +20,10 @@ import dev.nextftc.ftc.components.BulkReadComponent;
 
 @Configurable
 @Autonomous(name = "9 Ball Red Side Classifier Auto")
-public class RedClassifierAuto extends NextFTCOpMode{
+public class Red9BallClassifierAuto extends NextFTCOpMode{
 
-        public RedClassifierAuto() {
+        public Red9BallClassifierAuto() {
             addComponents(
-                    new SubsystemComponent(Intake.INSTANCE),
                     BulkReadComponent.INSTANCE,
                     new PedroComponent(Constants::createFollower)
             );
@@ -62,38 +57,37 @@ public class RedClassifierAuto extends NextFTCOpMode{
 
         private Path shoot1, intake1, goBack1, intake2, goBack2, park;
 
-        private final Pose startPose = new Pose(26.000, 129.000, Math.toRadians(-44)).mirror();
-        private final Pose scoringPose = new Pose(59.036, 108.578, Math.toRadians(-35)).mirror();
+        private final Pose startPose = new Pose(118.000, 130.500, Math.toRadians(216));
+        private final Pose scoringPose = new Pose(92.632, 107.117, Math.toRadians(225));
 
-        private final Pose intakePose1 = new Pose(19.354, 83.868, Math.toRadians(180)).mirror();
-        private final Pose intakeShortControlPose1 = new Pose(73.278, 98.597).mirror();
-        private final Pose intakeShortControlPose2 = new Pose(76.078, 87.398).mirror();
-        private final Pose intakeShortControlPose3 = new Pose(63.418, 85.085).mirror();
+        private final Pose intakePose1 = new Pose(129.150, 83.990, Math.toRadians(0));
+        private final Pose intakeShortControlPose1 = new Pose(74.982, 89.224);
+        private final Pose intakeShortControlPose2 = new Pose(69.992, 83.990);
 
-        private final Pose goBackShortControlPose1 = new Pose(75.347, 82.529).mirror();
+        private final Pose intakePose2 = new Pose(128.298, 59.523, Math.toRadians(0));
+        private final Pose intakeLongControlPose1 = new Pose(73.765, 89.102);
+        private final Pose intakeLongControlPose2 = new Pose(81.555, 58.306);
+        private final Pose intakeLongControlPose3 = new Pose(89.467, 59.645);
 
-        private final Pose intakePose2 = new Pose(18.259, 59.888, Math.toRadians(180)).mirror();
-        private final Pose intakeLongControlPose1 = new Pose(94.336, 84.964).mirror();
-        private final Pose intakeLongControlPose2 = new Pose(51.855, 62.688).mirror();
-        private final Pose intakeLongControlPose3 = new Pose(97.258, 59.645).mirror();
+        private final Pose goBackLongControlPose1 = new Pose(80.825, 70.965);
 
-        private final Pose endPose = new Pose(20.693, 69.992, Math.toRadians(0)).mirror();
+        private final Pose endPose = new Pose(125.620, 70.600, Math.toRadians(180));
 
 
         public void buildPaths() {
             shoot1 = new Path(new BezierLine(startPose, scoringPose));
             shoot1.setLinearHeadingInterpolation(startPose.getHeading(), scoringPose.getHeading());
 
-            intake1 = new Path(new BezierCurve(scoringPose, intakeShortControlPose1,intakeShortControlPose2, intakeShortControlPose3, intakePose1));
+            intake1 = new Path(new BezierCurve(scoringPose, intakeShortControlPose1,intakeShortControlPose2, intakePose1));
             intake1.setTangentHeadingInterpolation();
 
-            goBack1 = new Path(new BezierCurve(intakePose1, goBackShortControlPose1, scoringPose));
+            goBack1 = new Path(new BezierLine(intakePose1, scoringPose));
             goBack1.setLinearHeadingInterpolation(intakePose1.getHeading(), scoringPose.getHeading());
 
             intake2 = new Path(new BezierCurve(scoringPose, intakeLongControlPose1, intakeLongControlPose2, intakeLongControlPose3, intakePose2));
             intake2.setTangentHeadingInterpolation();
 
-            goBack2 = new Path(new BezierLine(intakePose2, scoringPose));
+            goBack2 = new Path(new BezierCurve(intakePose2, goBackLongControlPose1, scoringPose));
             goBack2.setLinearHeadingInterpolation(intakePose2.getHeading(), scoringPose.getHeading());
 
             park = new Path(new BezierLine(scoringPose, endPose));
