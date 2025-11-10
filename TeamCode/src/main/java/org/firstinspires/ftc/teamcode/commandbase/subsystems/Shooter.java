32d43aafd429e693d5f89a3fbb960ff43c6d152e
s0hum <sohum.p.gupta@gmail.com>
2025-11-09 18:00:38 -0800

    test new shooter PID + gradle updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/subsystems/Shooter.java
@@ -5,8 +5,11 @@ import static org.firstinspires.ftc.teamcode.tuning.Globals.*;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.WaitUntil;
+import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.controllable.MotorGroup;
@@ -24,11 +27,31 @@ public class Shooter implements Subsystem {
 
     MotorGroup shooterMotorGroup = new MotorGroup(shooterMotorLeft, shooterMotorRight); //create motor group
 
+    ControlSystem controller = ControlSystem.builder()
+            .velPid(0.011, 0, 0)
+            .basicFF(0.0005)
+            .build();
+
+
     @Override
     public void initialize() {
-        shooterMotorLeft.getMotor().setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        shooterMotorRight.getMotor().setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooterMotorLeft.zeroed();
+        shooterMotorRight.zeroed();
+        shooterMotorGroup.setPower(0);
     }
+
+    public Command shooterToTargetVelocity() {
+        return new LambdaCommand()
+                .setStart(() -> controller.setGoal(new KineticState(0, targetVelocity, 0)))
+                .setIsDone(() -> true);
+    }
+
+    public Command shooterNewOff() {
+        return new LambdaCommand()
+                .setStart(() -> controller.setGoal(new KineticState(0, shooterOffVelocity, 0)))
+                .setIsDone(() -> true);
+    }
+
     public Command shooterFarShoot = new SetPower(shooterMotorGroup, 1.1);
 
     public Command shooterClassifierShoot = new SetPower(shooterMotorGroup, 0.8);
@@ -44,7 +67,7 @@ public class Shooter implements Subsystem {
 
     @Override
     public void periodic() {
-
+        shooterMotorGroup.setPower(controller.calculate(shooterMotorGroup.getState()));
         ActiveOpMode.telemetry().addData("Right Shooter Motor Velocity:", shooterMotorRight.getVelocity());
         ActiveOpMode.telemetry().addData("Left Shooter Motor Velocity:", shooterMotorLeft.getVelocity());
         ActiveOpMode.telemetry().addData("Motor Group Velocity", shooterMotorGroup.getVelocity());
